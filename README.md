025eb48f76553a3ecdb17eb087f14511e075d4e6
Niran Fajemisin <nfajemisin@tandemseven.com>
2016-07-12 22:49:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# MoveNow - A Fitbit Notifier
+
+## Why
+The reason behind this example was that I needed to have a mechanism for reminding myself to regularly get some steps every hour. While this feature is supported by newer Fitbit devices, my [Fitbit Charge](https://www.fitbit.com/charge#features) was missing the _Reminders to Move_ function. My search to an approach to deploying this service without the need to manage servers etc lead me to giving [AWS Lambda][1] a try. AWS Lambda is a part of the AWS compute offering that lets one run code in response to events without having to provision or manage servers.
+
+
+## How does it work?
+The process for getting the _movenow_ notification occur through two related flows; see diagram below. The first flow pertains to capturing steps data from the Fitbit device. This part is automatically when the device is paired with a smartphone running the Fitbit app. The device will sync periodically (approx. every 15 minutes) with the app and as long as the smartphone is connected to the network it will upload the latest step data to the Fitbit server. 
+
+The second flow is the are that this example tackles. A scheduled rule is created in AWS CloudWatch to invoke the AWS Lambda function on an hourly basis. The AWS Lambda function retrieves step data from Fitbit servers and determines based on the step threshold value if the user needs to be notified. If a notification needs to be sent, then the AWS Lambda function will use AWS Simple Notification Service (SNS) to send an SMS text through a predefined topic in SNS.
+
+These two flows are depicted in the diagram below:    
+
+[[ADD DIAGRAM HERE!]]
+
+
+## Content
+
+The source code is based on a NodeJS package structure and uses NPM to import libraries that facilitate certain core functions. The following are the key files needed 
+- [src/lambda.js][2]: The lambda function to transform the payload
+- [package.json](package.json): Defines the packaging tasks and the npm modules in use
+
+## Requirement
+- Need to create [AWS account](http://docs.aws.amazon.com/lambda/latest/dg/setting-up.html)
+- Need to setup [AWS CLI](http://docs.aws.amazon.com/lambda/latest/dg/setup-awscli.html) with the credentials of your AWS account
+- NodeJS and NPM need to be installed 
+- Need to have an app [registered with Fitbit](https://dev.fitbit.com/apps) in order to use the [Web API](https://dev.fitbit.com/docs/basics/).
+- [Obtain an access token from Fitbit to invoke the API](https://dev.fitbit.com/docs/oauth2/). You will want to obtain this token ahead of time (providing it with a large expiration period) as it will be needed to invoke the Fitbit API to obtain step data.
+
+## Setting up the Components
+1. Define the [AWS Lambda function][2]
+2. [Create Amazon Simple Notification Service (SNS) Topic](http://docs.aws.amazon.com/sns/latest/dg/GettingStarted.html) through the console
+3. Create [AWS CloudWatch event rule](http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/WhatIsCloudWatchEvents.html). The rule should define the frequency of execution through a schedule (_fixed rate_ or Cron expression). The target should be the Lambda function created in _Step 1_  with the _Configure Input_ having a JSON object containing the configuration object (event input values) of the function. 
+
+## Working with the source code
+- Clone or fork this repository
+- Ensure that you've followed the [Requirements](#requirements) section above
+- Run `npm run build` to install dependencies, package the Lambda function and node modules in a zip and finally deploys the Lambda function to AWS using the AWS CLI.
+
+###License
+
+See [LICENSE](LICENSE) for further details.
+ 
+
+[1]: http://docs.aws.amazon.com/lambda/latest/dg/welcome.html
+[2]: ./src/lambda.js
+ 
---
+++ src/lambda.js
@@ -0,0 +1,153 @@
+/**
+ * AWS Lambda function that is periodically invoked to determine  if a Fitbit 
+ * user has been sedentary over over the last hour. The function retrieves 
+ * the Fitbit intra-day step count for the user up to the current time 
+ * period and notifies the user through the AWS Simple Notification Service (SNS) 
+ * to get moving.
+ *
+ * Event Flow:
+ * 1. AWS CloudWatch Event is triggered hourly using a defined Scheduled Rule
+ * 2. The scheduled rule invokes the AWS Lambda function 
+ * 3. The lambda function fetches the step count for the user for the past hour
+ * 4. If the hourly step goal is not met, a notification message is sent to 
+ *    AWS SNS
+ * 5. AWS SNS sends an SMS text message to the user to move around
+ *
+ * @param event AWS event object containing event data 
+ * @param  context execution context of the function  
+ * @param callback Optional callback to return information to the caller.
+  */
+
+exports.handler = function(event, context, callback) {
+	"use strict";
+ 
+ 	// Retrieve the access token from the invoker
+	const accessToken = event.accessToken;
+
+	// Retrieve the hourly step threshold (# of step needed to be met to avoid 
+	// considered to be sedentary)
+	const minHourlySteps = event.minHourlySteps;
+	// The Amazon resource name for the SNS topic
+	const snsTopicArn = event.snsTopicArn;
+	// The AWS region in which SNS topic resides   
+	const snsTopicRegion = event.snsTopicRegion;
+
+	// Base Fitbit URL
+	const fitbitBaseUrl = "https://api.fitbit.com/1/user/-/";
+
+	// Load the axios http lib.
+	const axios = require('axios');
+	// Promise lib.
+	const Promise = require('promise');
+
+
+	/*
+		1. Determine the Profile's UTC Offset
+		2. Get Intraday Steps
+		3. Compute current steps and send notification if applicable
+	*/
+	getFitbitProfileUTCOffset().
+	then(getIntradaySteps).
+	then(function(dataset) {
+		//Needs to be above our threshold
+		let stepCount = 0;
+
+		console.log(`dataset.length=${dataset.length}`);
+
+		for (let index = 0; index < dataset.length; ++index) {
+			stepCount += dataset[index].value;
+		}
+
+		if (stepCount < minHourlySteps) {
+			let message = `You've only move ${stepCount} this hour. Get movin'!` ;
+
+			console.log(message);
+
+		 	// AWS SDK
+			const AWS = require('aws-sdk');
+
+			AWS.config.update({
+			  'region': snsTopicRegion
+			});
+			
+			//create and configure a new SNS client 
+			let sns = new AWS.SNS();
+
+			// Init publish request parameters
+			let publishParams = { 
+    			TopicArn : snsTopicArn,
+    			Message: message
+  			};
+
+			/// Send SNS message
+  			sns.publish(publishParams, function(err, data) {
+    			console.log("MessageId - ${data.MessageId}");
+  			});		
+		}
+
+		callback(null, 'MoveNow Lambda function completed.');
+	}).catch(function (error) {
+		console.log('error', error);
+		callback(error);
+	});
+
+
+	/**
+	 * Determines the user's time offset from UTC in milliseconds. This allows us to normalized
+	 * the local time with the device's timezone. (at least the last recorded timezone)
+	 * @return the UTC offset in milliseconds
+	 */
+	function getFitbitProfileUTCOffset() {
+		return axios({
+			url: fitbitBaseUrl + "profile.json",
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer ' + accessToken
+			}
+		}).then(response => {
+			let offsetFromUTCMillis = response.data.user.offsetFromUTCMillis
+			console.log("User's UTC offest in milliseconds is: " + offsetFromUTCMillis);
+			return offsetFromUTCMillis;
+		}).catch(error => {
+			throw error;
+		});
+	}
+
+
+	/**
+	 * Get the intraday steps for the current user using the input UTC offet
+	 */
+	function getIntradaySteps(offsetFromUTCMillis) {
+		// Moment.js library
+		const moment = require('moment')
+
+		//Get the current date/time and offset based on the Fitbit user's profile
+		let now = moment();
+		now.add(offsetFromUTCMillis, 'ms');
+
+		//Format date range values
+		let hourString = now.get('hour');
+		let dateString = now.format("YYYY-MM-DD");
+
+		//URL to fetch data for the current hour
+		let url  = fitbitBaseUrl + `activities/steps/date/${dateString}/1d/15min/time/${hourString}:00/${hourString}:59.json`;
+
+		//Fetch the data from Fitbit
+		console.log("Invoking Fitbit endpoint at: " + url);
+
+		return axios({
+			url: url,
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer ' + accessToken
+			}
+		}).then(response => {
+			console.log("Returned payload: \n" + response.data["activities-steps-intraday"]);
+			let dataset = response.data["activities-steps-intraday"].dataset;
+			return dataset;
+		}).catch(error => {
+			throw error;
+		});
+	}
+
+}
